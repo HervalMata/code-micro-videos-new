9aaf94437fe6d68806b8bb490e9b4fed08a63096
HervalMata <hervalmataamparo@gmail.com>
2021-02-20 16:52:11 -0300

    refactor: :recycle: change id for uuid
    refactor model category for using uuid

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Traits\Uuid;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -9,9 +10,16 @@ class Category extends Model
 {
     use SoftDeletes;
 
+    use Uuid;
+
     protected $dates = ['deleted_at'];
-    
+
     protected $fillable = [
         'name', 'description', 'is_active'
+    ];
+
+    protected $casts = [
+        'id' => 'string',
+        'is_active' => 'boolean'
     ];
 }
