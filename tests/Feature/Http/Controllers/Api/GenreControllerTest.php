5329315a9bdfa1c72f0bdacc667648749e241d47
HervalMata <hervalmataamparo@gmail.com>
2021-02-22 19:09:55 -0300

    test: :recycle: refactor http test invalidation data

--- tests/Feature/Http/Controllers/Api/GenreControllerTest.php
+++ tests/Feature/Http/Controllers/Api/GenreControllerTest.php
@@ -9,10 +9,12 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\TestResponse;
 use Illuminate\Support\Facades\Lang;
 use App\Models\Genre;
+use Tests\Traits\TestValidations;
 
 class GenreControllerTest extends TestCase
 {
     use DatabaseMigrations;
+    use TestValidations;
 
     public function testIndex()
     {
@@ -62,30 +64,19 @@ class GenreControllerTest extends TestCase
 
     private function assertInvalidationMax(TestResponse $response)
     {
-        $response->assertStatus(422)
-            ->assertJsonValidationErrors(['name'])
-            ->assertJsonFragment([
-                Lang::get('validation.max.string', ['attribute' => 'name', 'max' => 255])
-            ]);
+        $this->assertInvalidationFields($response, ['name'], 'max.string', ['max' => 255]);
     }
 
     private function assertInvalidationBoolean(TestResponse $response)
     {
-        $response->assertStatus(422)
-            ->assertJsonValidationErrors(['is_active'])
-            ->assertJsonFragment([
-                Lang::get('validation.boolean', ['attribute' => 'is active'])
-            ]);
+        $this->assertInvalidationFields($response, ['is_active'], 'boolean');
     }
 
     private function assertInvalidationRequired(TestResponse $response)
     {
-        $response->assertStatus(422)
-            ->assertJsonValidationErrors(['name'])
-            ->assertJsonMissingValidationErrors(['is_valid'])
-            ->assertJsonFragment([
-                Lang::get('validation.required', ['attribute' => 'name'])
-            ]);
+        $this->assertInvalidationFields($response, ['name'], 'required');
+
+        $response->assertJsonMissingValidationErrors(['is_valid']);
     }
 
     public function testStore()
