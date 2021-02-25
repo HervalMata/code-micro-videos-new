89a56ca20dd0ea3f973a9613067bf151362e2dd6
HervalMata <hervalmataamparo@gmail.com>
2021-02-24 22:04:15 -0300

    feat:art:add category relation in controller for genre
    created structure for category relation for genre

--- app/Http/Controllers/Api/GenreController.php
+++ app/Http/Controllers/Api/GenreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Api\BasicCrudController;
 use App\Models\Genre;
+use Illuminate\Http\Request;
 
 class GenreController extends BasicCrudController
 {
@@ -16,6 +17,7 @@ class GenreController extends BasicCrudController
     {
         return [
             'name' => 'required|max:255',
+            'categories_id' => 'required|array|exists:categories,id,deleted_at,NULL',
             'is_active' => 'boolean'
         ];
     }
@@ -23,5 +25,38 @@ class GenreController extends BasicCrudController
     protected function rulesUpdate()
     {
         return $this->rulesStore();
+    }
+
+    public function store(Request $request)
+    {
+        $validateData = $this->validate($request, $this->rulesStore());
+        $self = $this;
+        $obj = \DB::transaction(function () use ($request, $validateData, $self) {
+            $obj = $this->model()::create($validateData);
+            $self->handleRelations($obj, $request);
+            return $obj;
+        });
+
+        $obj->refresh();
+        return $obj;
+    }
+
+    public function update(Request $request, $id)
+    {
+        $obj = $this->findOrFail($id);
+        $validateData = $this->validate($request, $this->rulesUpdate());
+        $self = $this;
+        $obj = \DB::transaction(function () use ($request, $validateData, $self, $obj) {
+            $obj->update($validateData);
+            $self->handleRelations($obj, $request);
+            return $obj;
+        });
+
+        return $obj;
+    }
+
+    protected function handleRelations($obj, Request $request)
+    {
+        $obj->categories()->sync($request->get('categories_id'));
     }
 }
